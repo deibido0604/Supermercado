c40aa185067dea6bfbc3daf433c0b50591d9473c
DAVID JARED TORRES ROMERO <jared.torres@unah.hn>
2021-09-23 23:15:13 -0600

    prueba

--- Supermercado/WinSupermercado/Form1.Designer.cs
+++ Supermercado/WinSupermercado/Form1.Designer.cs
@@ -91,7 +91,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(50, 286);
+            this.button1.Location = new System.Drawing.Point(50, 268);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 33);
