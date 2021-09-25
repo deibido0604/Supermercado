7c972d19b49b6fd4706f76c62971d97b33c2b04e
Irma Mejia <cursosdecomputacion.net@gmail.com>
2021-09-24 19:57:18 -0700

    Cambio en el login Aracely

--- Supermercado/WinSupermercado/FormLogin.Designer.cs
+++ Supermercado/WinSupermercado/FormLogin.Designer.cs
@@ -60,10 +60,10 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(66, 360);
+            this.textBox2.Location = new System.Drawing.Point(77, 360);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(208, 20);
+            this.textBox2.Size = new System.Drawing.Size(197, 20);
             this.textBox2.TabIndex = 4;
             // 
             // button1
