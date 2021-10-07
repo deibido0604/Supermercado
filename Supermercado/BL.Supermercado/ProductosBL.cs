0085d3bd102542d7badd63f89644d4d1991477cc
user <user@DESKTOP-6E02UC2>
2021-10-07 14:43:00 -0600

    ClaseModelo-Producto

---
+++ Supermercado/BL.Supermercado/ProductosBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Supermercado
+{
+    public class ProductosBL
+    {
+        //propiedad 
+        BindingList<Producto> ListaProductos;
+        //constructor
+        public ProductosBL()
+        {
+
+        }
+    }
+    public class Producto
+    {//propiedades de la clase producto
+        public int id { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Existencia { get; set; }
+        public bool Activo { get; set; }
+    }
+}
