0b766ed27225260874aaa24a428cfc5a4158937a
DAVID JARED TORRES ROMERO <jared.torres@unah.hn>
2021-10-20 00:20:01 -0600

    Video 22

--- Supermercado/BL.Supermercado/ProductosBL.cs
+++ Supermercado/BL.Supermercado/ProductosBL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,56 +10,24 @@ namespace BL.Supermercado
 {
     public class ProductosBL
     {
+        Contexto _contexto;
         //propiedad 
         public BindingList<Producto>ListaProductos { get; set; }
         //constructor
         public ProductosBL()
         {
+            
+            _contexto = new Contexto();
+
             ListaProductos = new BindingList<Producto>();
-
-            var producto1 = new Producto();
-            producto1.id = 1;
-            producto1.Descripcion = "Arroz";
-            producto1.PrecioUnidad = 10;
-            producto1.Existencia = 2;
-            producto1.Activo = true;
-            producto1.ProductoNacional = true;
-            producto1.ProductoExtranjero = false;
-            ListaProductos.Add(producto1);
-
-            var producto2 = new Producto();
-            producto2.id = 2;
-            producto2.Descripcion = "Frijoles";
-            producto2.PrecioUnidad = 60;
-            producto2.Existencia = 5;
-            producto2.Activo = true;
-            producto2.ProductoNacional = true;
-            producto2.ProductoExtranjero = false;
-            ListaProductos.Add(producto2);
-
-            var producto3 = new Producto();
-            producto3.id = 3;
-            producto3.Descripcion = "Huevos";
-            producto3.PrecioUnidad = 3;
-            producto3.Existencia = 89;
-            producto3.Activo = true;
-            producto3.ProductoNacional = true;
-            producto3.ProductoExtranjero = false;
-            ListaProductos.Add(producto3);
-
-            var producto4 = new Producto();
-            producto4.id = 4;
-            producto4.Descripcion = "Queso";
-            producto4.PrecioUnidad = 35;
-            producto4.Existencia = 245;
-            producto4.Activo = true;
-            producto4.ProductoNacional = true;
-            producto4.ProductoExtranjero = false;
-            ListaProductos.Add(producto4);
+            
         }
 
         public BindingList<Producto> ObtenerProductos()
         {
+            _contexto.Productos.Load();
+            ListaProductos = _contexto.Productos.Local.ToBidingList();
+
             return ListaProductos;
         }
 
