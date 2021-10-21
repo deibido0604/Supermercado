fe897032f390c453bae8b103f10e6783573399a2
DAVID JARED TORRES ROMERO <jared.torres@unah.hn>
2021-10-20 22:58:12 -0600

    Asignando nombre de base de datos y configuracion - David

--- Supermercado/BL.Supermercado/Contexto.cs
+++ Supermercado/BL.Supermercado/Contexto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,17 @@ namespace BL.Supermercado
 {
     public class Contexto: DbContext 
     {
-        public Contexto(): base()
+        public Contexto(): base("Productos Supermercado")
         {
 
         }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+
 
         public DbSet<Producto> Productos { get; set; }
     }
