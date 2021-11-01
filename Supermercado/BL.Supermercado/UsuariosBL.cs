2f61cf073264700373afba27e4822bf74a5447eb
DAVID JARED TORRES ROMERO <jared.torres@unah.hn>
2021-10-31 23:30:36 -0600

    cambio mal

--- Supermercado/BL.Supermercado/UsuariosBL.cs
+++ Supermercado/BL.Supermercado/UsuariosBL.cs
@@ -3,6 +3,6 @@
     public class Usuario
     {
         public string Contrasena { get; internal set; }
-        public string Nombre { get; internal set; }
+        public string Nombre { get; internal set; } 
     }
 }
