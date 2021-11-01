3772e2a78e70e79d895dd179812aa3e18cc1c5f7
user <user@DESKTOP-6E02UC2>
2021-10-31 23:58:53 -0600

    a

--- Supermercado/WinSupermercado/FormProductos.Designer.cs
+++ Supermercado/WinSupermercado/FormProductos.Designer.cs
@@ -106,7 +106,7 @@
             // 
             existenciaLabel.AutoSize = true;
             existenciaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            existenciaLabel.Location = new System.Drawing.Point(287, 107);
+            existenciaLabel.Location = new System.Drawing.Point(287, 104);
             existenciaLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             existenciaLabel.Name = "existenciaLabel";
             existenciaLabel.Size = new System.Drawing.Size(91, 20);
