5c16b83afcac134e29fb5a5794a11fda8149a24e
DAVID JARED TORRES ROMERO <jared.torres@unah.hn>
2021-10-31 23:33:52 -0600

    borrabdo Clase Usuarios

--- Supermercado/BL.Supermercado/UsuariosBL.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BL.Supermercado
-{
-    public class Usuario
-    {
-        public string Contrasena { get; internal set; }
-        public string Nombre { get; internal set; } 
-    }
-}
