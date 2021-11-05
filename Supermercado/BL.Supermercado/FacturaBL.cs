4626b5aeaf7a6347c8e0ea17933ada5853f9163a
DAVID JARED TORRES ROMERO <jared.torres@unah.hn>
2021-11-04 18:54:48 -0600

    Cambio David

--- Supermercado/BL.Supermercado/FacturaBL.cs
+++ Supermercado/BL.Supermercado/FacturaBL.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
@@ -22,7 +23,7 @@ namespace BL.Supermercado
 
         public BindingList<Factura> ObtenerFacturas()
         {//Factura
-            _contexto.Facturas.Include("FacturaDetalle").Load();
+            _contexto.Facturas.Include("Factura Detalle").Load();
             ListaFacturas = _contexto.Facturas.Local.ToBindingList();
 
             return ListaFacturas;
@@ -62,7 +63,8 @@ namespace BL.Supermercado
             return resultado;
         }
     }
-        public class Factura
+      
+     public class Factura
     {
 
         public int Id { get; set; }
