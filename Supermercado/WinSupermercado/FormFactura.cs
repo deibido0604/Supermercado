10c228817f64e4c78a2b3756b9109635891ba500
BRayan1582 <brayan1uf@gmail.com>
2021-11-06 10:40:04 -0600

    FormFactura

---
+++ Supermercado/WinSupermercado/FormFactura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSupermercado
+{
+    public partial class FormFactura : Form
+    {
+        public FormFactura()
+        {
+            InitializeComponent();
+        }
+
+        private void FormFactura_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
