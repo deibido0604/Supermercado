fd7e4bf9089f53b6f09d3e89c5647d942ee0d449
AracelyMN <91372015+AracelyMN@users.noreply.github.com>
2022-01-29 17:34:50 -0800

    prueba

--- Supermercado/WinSupermercado/FormUsuarios.Designer.cs
+++ Supermercado/WinSupermercado/FormUsuarios.Designer.cs
@@ -484,7 +484,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(487, 50);
+            this.label1.Location = new System.Drawing.Point(382, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(130, 31);
             this.label1.TabIndex = 27;
